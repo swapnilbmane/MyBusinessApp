52f433b8e3fde0f0522bbe88969a54aa1cfec49b
swapnilbmane <swapnilbmane@gmail.com>
2017-11-09 23:46:10 +0530

    Added vs default mvc application

---
+++ src/MyBusinessApp/MyBusinessApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBusinessApp.Startup))]
+namespace MyBusinessApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
